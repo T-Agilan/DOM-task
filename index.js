79ad6020ea565aba65ef52f6b787b99cc1db2918
T-Agilan <agilanakil1432@gmail.com>
2023-08-02 14:05:52 +0530

    delete

--- index.js
+++
@@ -1,7 +0,0 @@
-
-      function convert() {
-        const a = Number(document.getElementById("input").value);
-        const b = a /2.54
-        document.getElementById("result").innerHTML =  b.toFixed(2) + "inches";
-      }
-    
